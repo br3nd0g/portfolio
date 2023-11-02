ecc5fe5b31fa072cd6fcc2ddf72b934cee47ec74
brendan :) <brendan.j.corcoran@gmail.com>
2023-11-02 23:46:20 +0100

    yeahhh baby

---
+++ scriptnew.js
@@ -0,0 +1,23 @@
+const skillsObj = {
+    "languages": {
+        "JavaScript": 1
+    },
+    "libraries/frameworks": {
+        "JavaScript": {
+            "Selenium": 1,
+            "Vue.Js": 1
+        }
+    },
+    "areas": {
+        "Automation Testing": 1
+    },
+    "interests": {
+        "general": {
+
+        },
+        "currentlyLearning": {
+            
+        }
+    }
+};
+
