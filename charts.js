616e332d1fb930ca032d120ffd95c3cecbcb4061
brendan :) <brendan.j.corcoran@gmail.com>
2024-06-20 15:21:58 +0200

    added some more stuff bruh!

--- charts.js
+++ charts.js
@@ -1,5 +1,21 @@
+//morphing data from objects in to Highcharts-friendly data
+const langCategories = [];
+const langData = [];
+const libraryCategories = [];
+const libraryData = [];
+
+for(let i=0;i < skillsObj.languages.length;i++) langCategories.push(skillsObj.languages[i].name);
+for(let i=0;i < skillsObj.languages.length;i++) langData.push(skillsObj.languages[i].y);
+for(let i=0;i < skillsObj["libraries/frameworks"].length;i++) libraryCategories.push(skillsObj["libraries/frameworks"][i].name);
+for(let i=0;i < skillsObj["libraries/frameworks"].length;i++) libraryData.push(skillsObj["libraries/frameworks"][i].y);
+
+// keeping chart global for reference in later function
+var lalChart;
+
+
 document.addEventListener('DOMContentLoaded', function () {
-    const chart = Highcharts.chart('bubbleContainer', {
+
+    const aoeChart = Highcharts.chart('bubbleContainer', {
         chart: {
             type: 'packedbubble',
             backgroundColor: null
@@ -41,5 +57,106 @@ document.addEventListener('DOMContentLoaded', function () {
         }]
     });
 
-    console.log("Bubble Chart Loaded...")
+    // languages and libraries graph
+    lalChart = remakeLALgraph(langCategories, langData);
 });
+
+function remakeLALgraph(ctgry, lalData){
+    lalChart = Highcharts.chart('langBarGraph', {
+        chart: {
+            type: 'bar',
+            backgroundColor: null
+        },
+
+        accessibility: {
+            enabled: false
+        },
+        tooltip: {
+            enabled: false,
+        },
+        legend: {
+            enabled: false
+        },
+        credits: {
+            enabled: false
+        },
+        title: {
+            text: ""
+        },
+
+        xAxis: {
+            categories: ctgry,
+            labels: {
+                align:'left',
+                x:5,
+                style: {
+                    fontSize: '1em',
+                    color:'#000'
+                }
+            },
+            tickLength: 0,
+            lineColor: '#595959',
+           
+        },
+        yAxis: {
+            allowDecimals:false,
+            title: {
+                text: 'Proficiency (out of 5)',
+                style: {
+                    fontSize:'12px',
+                    fontWeight: 'bold' 
+                }
+            },
+            min: 0,
+            max: 5,
+        },
+
+        plotOptions: {
+            bar: {
+                borderRadiusTopLeft: '50%',
+                borderRadiusTopRight: '50%',
+                dataLabels: {
+                    enabled: true
+                },
+                groupPadding: 0.1,
+                borderColor: "#595959",
+            },
+        },
+
+        series: [{
+            name: 'Languages Experience',
+            data: lalData,
+            color: "#91C499",
+        }]
+    });
+}
+
+function switchGraph(e){
+
+    if(e.classList.contains("inactiveOption")){
+        const librariesClicked = e.id === "librariesOption";
+
+        if(librariesClicked){
+            e.classList.remove("inactiveOption");
+            document.getElementById("languagesOption").classList.add("inactiveOption");
+
+            document.getElementById('langBarGraph').innerHTML = '';
+            remakeLALgraph(libraryCategories, libraryData);
+
+            // lalChart.xAxis[0].setCategories(libraryCategories);
+            // lalChart.series[0].setData(libraryData);
+            // lalChart.redraw();
+        }
+        else{
+            e.classList.remove("inactiveOption");
+            document.getElementById("librariesOption").classList.add("inactiveOption");
+
+            document.getElementById('langBarGraph').innerHTML = '';
+            remakeLALgraph(langCategories, langData);
+
+            // lalChart.xAxis[0].setCategories(langCategories);
+            // lalChart.series[0].setData(langData);
+            // lalChart.redraw();
+        }
+    }
+}
--- scriptnew.js
+++ scriptnew.js
@@ -1,28 +1,83 @@
 const skillsObj = {
-    "languages": {
-        "JavaScript": 1
-    },
-    "libraries/frameworks": {
-        "JavaScript": {
-            "Selenium": 1,
-            "Vue.Js": 1
+    "libraries/frameworks": [
+        {
+            name: "Selenium",
+            y: 2.5
+        },
+        {
+            name: "Vue.js",
+            y: 2
+        },
+        {
+            name: "React.js",
+            y: 3
+        },
+        {
+            name: "Flask",
+            y: 4
+        },
+        {
+            name: "Pandas",
+            y: 3.5
+        },
+        {
+            name: "Numpy",
+            y: 3
+        },
+        {
+            name: "Matplotlib",
+            y: 3
+        },
+        {
+            name: "Highcharts",
+            y: 3
         }
-    },
-    "areas": [
+    ],
+    languages: [
+        {
+            name: "JavaScript",
+            y: 4
+        },
+        {
+            name: "Python",
+            y: 4
+        },
+        {
+            name: "Lua",
+            y: 2
+        },
+        {
+            name: "C++",
+            y: 1
+        },
+        {
+            name: "C#",
+            y: 2
+        },
+        {
+            name: "Node.js",
+            y: 3.5
+        },
+        {
+            name: "HTML/CSS",
+            y: 4
+        }
+    ],
+    areas: [
         {
             name: "Automation Testing",
             value: 1
         },
         {
             name: "Test2",
-            value: 4
+            value: 1
         },
         {
             name: "Test3",
             value: 2
         },
     ],
-    "interests": {
+    interests: {
         "general": {
 
         },
@@ -31,4 +86,3 @@ const skillsObj = {
         }
     }
 };
-
